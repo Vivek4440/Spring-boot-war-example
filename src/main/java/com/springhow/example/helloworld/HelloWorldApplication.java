f1d2dfbcbc8c10a7dc502dc7ca8b5a0904ffdcc2
Vivek4440 <107357023+Vivek4440@users.noreply.github.com>
2022-06-19 01:18:48 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "this is third time you guys are doing pipeline so wait !!";
+        return "this is fourth time you guys are doing pipeline so wait !!";
     }
 
 }
